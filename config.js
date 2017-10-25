bae1a4a5ef66d13b1bfe0df5685a4c743c3e9419
Wyatt Johnson <wyattjoh@gmail.com>
2017-10-25 17:03:07 -0600

    Added some docs to the config

--- config.js
+++ config.js
@@ -6,41 +6,50 @@ const convict = require('convict');
 // Load the configuration.
 const config = convict({
   host: {
+    doc: 'The interface to bind the application to',
     format: 'ipaddress',
     default: '127.0.0.1',
     env: 'HOST',
   },
   env: {
+    doc: 'The NODE_ENV environment',
     format: ['production', 'development'],
     default: 'development',
     env: 'NODE_ENV',
   },
   verbose_logs: {
+    doc: 'Enables verbose logging',
     format: 'Boolean',
     env: 'VERBOSE_LOGS',
     default: true,
   },
   port: {
+    doc: 'The port to bind the application to',
     format: 'port_or_windows_named_pipe',
     default: 5000,
     env: 'PORT',
   },
   session_secret: {
+    doc: 'The secret used to sign cookies and tokens',
     format: '*',
     env: 'SESSION_SECRET',
     default: null,
   },
   mongo_url: {
+    doc: 'URL to the MongoDB instance',
     format: '*',
     env: 'MONGO_URL',
     default: 'mongodb://localhost:27017/coralproject_slack',
   },
   redis_url: {
+    doc: 'URL to the Redis instance',
     format: '*',
     env: 'REDIS_URL',
     default: 'redis://localhost:6379',
   },
   root_url: {
+    doc:
+      'Public facing url where the application is hosted, with scheme and trailing slash',
     format: 'url',
     env: 'ROOT_URL',
     default: 'http://127.0.0.1:5000/',
