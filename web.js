34863d9d5d1fefd57efc79bdf57c639d1a695d07
Wyatt Johnson <wyattjoh@gmail.com>
2017-10-25 19:50:32 -0600

    added proxy trust

--- web.js
+++ web.js
@@ -12,6 +12,7 @@ const app = express();
 // View engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
 
 // Application wide middleware.
 app.use(middleware.logging.log);
